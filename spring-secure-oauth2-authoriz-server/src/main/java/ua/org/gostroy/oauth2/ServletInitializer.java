84d44326ce09786738a8291855a552bda39abe61
panser <gostroy@gmail.com>
2016-05-21 07:56:50 +0300

    init project spring-secure-oauth2-authoriz-server

---
+++ spring-secure-oauth2-authoriz-server/src/main/java/ua/org/gostroy/oauth2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ua.org.gostroy.oauth2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringSecureOauth2AuthorizServerApplication.class);
+	}
+
+}
