ade5ce439fc7f36bb974879be73f3b6a4699e48d
panser <gostroy@gmail.com>
2016-05-22 09:13:39 +0300

    add oauth2 client configuration from here https://github.com/spring-projects/spring-security-oauth/blob/3e1d0e0398ad2a24afe4a6d05602353a6c372ce3/tests/annotation/vanilla/src/main/java/demo/Application.java#L67-L67

--- spring-secure-oauth2-authoriz-server/src/main/java/ua/org/gostroy/oauth2/config/SpringSecurityOauth2.java
+++ spring-secure-oauth2-authoriz-server/src/main/java/ua/org/gostroy/oauth2/config/SpringSecurityOauth2.java
@@ -1,8 +1,14 @@
 package ua.org.gostroy.oauth2.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 /**
  * Created by Sergey on 5/21/2016.
@@ -10,5 +16,46 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @Configuration
 @EnableAuthorizationServer
 @EnableResourceServer
-public class SpringSecurityOauth2 {
+public class SpringSecurityOauth2 extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security.checkTokenAccess("isAuthenticated()");
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        // @formatter:off
+        clients.inMemory()
+                .withClient("my-trusted-client")
+                .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
+                .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
+                .scopes("read", "write", "trust")
+                .resourceIds("oauth2-resource")
+                .accessTokenValiditySeconds(600)
+                .and()
+                .withClient("my-client-with-registered-redirect")
+                .authorizedGrantTypes("authorization_code")
+                .authorities("ROLE_CLIENT")
+                .scopes("read", "trust")
+                .resourceIds("oauth2-resource")
+                .redirectUris("http://anywhere?key=value")
+                .and()
+                .withClient("my-client-with-secret")
+                .authorizedGrantTypes("client_credentials", "password")
+                .authorities("ROLE_CLIENT")
+                .scopes("read")
+                .resourceIds("oauth2-resource")
+                .secret("secret");
+        // @formatter:on
+    }
+
 }
