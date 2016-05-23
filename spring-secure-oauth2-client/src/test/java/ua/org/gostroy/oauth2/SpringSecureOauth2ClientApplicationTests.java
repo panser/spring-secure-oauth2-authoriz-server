9aea44d21cb8ad5e992f26a19c9f78ca89ae4839
panser <gostroy@gmail.com>
2016-05-23 17:14:06 +0300

    add spring-secure-oauth2-client app

---
+++ spring-secure-oauth2-client/src/test/java/ua/org/gostroy/oauth2/SpringSecureOauth2ClientApplicationTests.java
@@ -0,0 +1,18 @@
+package ua.org.gostroy.oauth2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringSecureOauth2ClientApplication.class)
+@WebAppConfiguration
+public class SpringSecureOauth2ClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
