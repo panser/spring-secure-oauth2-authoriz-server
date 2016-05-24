96d506fcba928ffe633b1fa1ac6951ed1bcec1c3
panser <gostroy@gmail.com>
2016-05-24 15:02:29 +0300

    make config for resource server more detailed

--- spring-secure-oauth2-resource-server/src/main/java/ua/org/gostroy/oauth2/resourceserver/config/SpringSecurityOauth2.java
+++ spring-secure-oauth2-resource-server/src/main/java/ua/org/gostroy/oauth2/resourceserver/config/SpringSecurityOauth2.java
@@ -1,12 +1,22 @@
 package ua.org.gostroy.oauth2.resourceserver.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 /**
  * Created by Sergey on 5/21/2016.
  */
 @Configuration
 @EnableResourceServer
-public class SpringSecurityOauth2 {
+public class SpringSecurityOauth2 extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        http
+            .antMatcher("/me")
+            .authorizeRequests().anyRequest().authenticated();
+    }
 }
