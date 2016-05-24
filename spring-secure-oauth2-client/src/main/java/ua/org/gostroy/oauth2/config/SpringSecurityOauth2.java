bb2e7f74046d774b9856b4ae3ab2e92720c3023a
panser <gostroy@gmail.com>
2016-05-24 11:34:43 +0300

    spring-secure-oauth2-client: adds a logout link as well for authenticated users.

--- spring-secure-oauth2-client/src/main/java/ua/org/gostroy/oauth2/config/SpringSecurityOauth2.java
+++ spring-secure-oauth2-client/src/main/java/ua/org/gostroy/oauth2/config/SpringSecurityOauth2.java
@@ -4,6 +4,20 @@ import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by Sergey on 5/21/2016.
@@ -20,6 +34,39 @@ public class SpringSecurityOauth2 extends WebSecurityConfigurerAdapter {
                 .antMatchers("/","/login**","/webjars/**")
                     .permitAll()
                 .anyRequest()
-                    .authenticated();
+                    .authenticated()
+            .and()
+                .logout().logoutSuccessUrl("/").permitAll()
+            .and()
+                .csrf().csrfTokenRepository(csrfTokenRepository())
+            .and()
+                .addFilterAfter(csrfHeaderFilter(), CsrfFilter.class)
+        ;
+    }
+
+    private CsrfTokenRepository csrfTokenRepository() {
+        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+        repository.setHeaderName("X-XSRF-TOKEN");
+        return repository;
+    }
+
+    private Filter csrfHeaderFilter() {
+        return new OncePerRequestFilter() {
+            @Override
+            protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                            FilterChain filterChain) throws ServletException, IOException {
+                CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+                if (csrf != null) {
+                    Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+                    String token = csrf.getToken();
+                    if (cookie == null || token != null && !token.equals(cookie.getValue())) {
+                        cookie = new Cookie("XSRF-TOKEN", token);
+                        cookie.setPath("/");
+                        response.addCookie(cookie);
+                    }
+                }
+                filterChain.doFilter(request, response);
+            }
+        };
     }
 }
