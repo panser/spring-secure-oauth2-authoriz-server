ea9c2552666edd1aaae2943624655b9cb06eeea3
panser <gostroy@gmail.com>
2016-05-24 12:42:10 +0300

    spring-secure-oauth2-client-facebook: change @EnableOAuth2Sso to manual spring-security-oauth2 configuration

--- spring-secure-oauth2-client-facebook/src/main/java/ua/org/gostroy/oauth2/config/SpringSecurityOauth2.java
+++ spring-secure-oauth2-client-facebook/src/main/java/ua/org/gostroy/oauth2/config/SpringSecurityOauth2.java
@@ -1,9 +1,23 @@
 package ua.org.gostroy.oauth2.config;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
@@ -23,8 +37,31 @@ import java.io.IOException;
  * Created by Sergey on 5/21/2016.
  */
 @Configuration
-@EnableOAuth2Sso
+@EnableOAuth2Client
 public class SpringSecurityOauth2 extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    OAuth2ClientContext oauth2ClientContext;
+
+    @Bean
+    @ConfigurationProperties("facebook.client")
+    OAuth2ProtectedResourceDetails facebook() {
+        return new AuthorizationCodeResourceDetails();
+    }
+
+    @Bean
+    @ConfigurationProperties("facebook.resource")
+    ResourceServerProperties facebookResource() {
+        return new ResourceServerProperties();
+    }
+
+    @Bean
+    public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(filter);
+        registration.setOrder(-100);
+        return registration;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -36,11 +73,14 @@ public class SpringSecurityOauth2 extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                     .authenticated()
             .and()
+                .exceptionHandling().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"))
+            .and()
                 .logout().logoutSuccessUrl("/").permitAll()
             .and()
                 .csrf().csrfTokenRepository(csrfTokenRepository())
             .and()
                 .addFilterAfter(csrfHeaderFilter(), CsrfFilter.class)
+                .addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
         ;
     }
 
@@ -68,5 +108,13 @@ public class SpringSecurityOauth2 extends WebSecurityConfigurerAdapter {
                 filterChain.doFilter(request, response);
             }
         };
+    }
+
+    private Filter ssoFilter() {
+        OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/facebook");
+        OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(facebook(), oauth2ClientContext);
+        facebookFilter.setRestTemplate(facebookTemplate);
+        facebookFilter.setTokenServices(new UserInfoTokenServices(facebookResource().getUserInfoUri(), facebook().getClientId()));
+        return facebookFilter;
     }
 }
