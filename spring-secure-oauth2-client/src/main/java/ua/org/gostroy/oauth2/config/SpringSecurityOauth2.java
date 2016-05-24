70c7e1985bb7d7476b4a1902b34e7bdaa2aa2ad0
panser <gostroy@gmail.com>
2016-05-24 10:27:07 +0300

    change spring-secure-oauth2-client to oauth2 client for facebook

---
+++ spring-secure-oauth2-client/src/main/java/ua/org/gostroy/oauth2/config/SpringSecurityOauth2.java
@@ -0,0 +1,13 @@
+package ua.org.gostroy.oauth2.config;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Sergey on 5/21/2016.
+ */
+@Configuration
+@EnableOAuth2Sso
+public class SpringSecurityOauth2 {
+
+}
