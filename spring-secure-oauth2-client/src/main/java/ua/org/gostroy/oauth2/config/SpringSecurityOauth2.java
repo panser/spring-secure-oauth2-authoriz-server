7b9e1b144170825b81d36f86a8ce245a7bdb9248
panser <gostroy@gmail.com>
2016-05-24 10:46:58 +0300

    spring-secure-oauth2-client: Add a Welcome Page

--- spring-secure-oauth2-client/src/main/java/ua/org/gostroy/oauth2/config/SpringSecurityOauth2.java
+++ spring-secure-oauth2-client/src/main/java/ua/org/gostroy/oauth2/config/SpringSecurityOauth2.java
@@ -2,12 +2,24 @@ package ua.org.gostroy.oauth2.config;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Created by Sergey on 5/21/2016.
  */
 @Configuration
 @EnableOAuth2Sso
-public class SpringSecurityOauth2 {
+public class SpringSecurityOauth2 extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                    .authorizeRequests()
+                .antMatchers("/","/login**","/webjars/**")
+                    .permitAll()
+                .anyRequest()
+                    .authenticated();
+    }
 }
